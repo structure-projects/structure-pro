2412f9f946574835f45b2ce68ee4be253b3269d9
chuck <361648887@qq.com>
2026-05-10 04:17:59 +0800

    chore(gateway): 删除网关模块的所有代码文件
    - 删除了网关应用主类 GatewayApplication.java
    - 删除了网关常量定义类 GatewayConstants.java
    - 删除了全局网关过滤器 GlobalGatewayFilter.java
    - 删除了RabbitMQ配置类 RabbitMQConfig.java
    - 删除了Redis配置类 RedisConfig.java
    - 删除了重放攻击防护过滤器 ReplayAttackPreventionFilter.java
    - 删除了路由定位器配置类 RouteLocatorConfig.java
    - 删除了网关配置属性类 StructureGatewayProperties.java
    - 删除了租户识别过滤器 TenantIdentificationFilter.java
    - 删除了主配置文件 application.yaml 和 bootstrap.yaml
    - 删除了网关模块的 pom.xml 文件
    - 删除了监控中心的测试类 JavaTest.java 和 package-info.java
    - 删除了父项目的 pom.xml 配置文件

--- structure-cloud-gateway/src/main/java/cn/structured/cloud/gateway/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * @author chuck
- * @version 2024/07/25 下午10:05
- * @since 1.8
- */
-package cn.structured.cloud.gateway;
--- structure-monitoring-center/src/main/java/cn/structured/monitoring/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * @author chuck
- * @version 2024/08/04 下午11:50
- * @since 1.8
- */
-package cn.structured.monitoring;
